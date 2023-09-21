3550bbb26b4a15e0814a74b23c69211eded189a6
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2023-09-20 22:32:37 -0300

    feat(js)

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
+// let numeroVerdadero = Math.floor(Math.random() * (10 - 1 + 1) + 1)
+// alert(numeroVerdadero)
+// let contadorIntentos = 0
+
+// function comprobarNumero() {
+//    let num = document.getElementById("numeroIngresado").value
+//    let p = document.getElementById("parrafoIntentos")
+//    contadorIntentos++
+
+//    if (parseInt(num) == numeroVerdadero) {
+//       alert("¡¡ADIVINASTE EL NÚMERO!!")
+//       p.innerHTML = "Cantidad de intentos: " + contadorIntentos;
+//    }
+//    else {
+//       alert("No es el número :( ")
+//       p.innerHTML = "Cantidad de intentos: " + contadorIntentos;
+//    }
+// }
+
+
 let numeroVerdadero = Math.floor(Math.random() * (10 - 1 + 1) + 1)
 alert(numeroVerdadero)
-let contadorIntentos = 0
 
-function comprobarNumero() {
-   let num = document.getElementById("numeroIngresado").value
-   let p = document.getElementById("parrafoIntentos")
-   contadorIntentos++
-
-   if (parseInt(num) == numeroVerdadero) {
+for (i = 0; i <= 5; i++) {
+   let num = parseInt(prompt("Adivina el numero"))
+   if (num == numeroVerdadero) {
       alert("¡¡ADIVINASTE EL NÚMERO!!")
-      p.innerHTML = "Cantidad de intentos: " + contadorIntentos;
-   }
-   else {
+      break;
+   } else {
       alert("No es el número :( ")
-      p.innerHTML = "Cantidad de intentos: " + contadorIntentos;
    }
 }
-
-for (let i=0; i==numeroVerdadero; i++)
-
 
 
 
